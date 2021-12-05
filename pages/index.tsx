64067d16034168fb43627427974d5ef2b44fe87b
Ahmad Nader <ahmadnaderx1998@gmail.com>
2021-12-05 22:08:14 +0200

    feat: add storybook, downgrade tailwind for storybook postcss support

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,11 @@ import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <p>We play what we are dealt</p>
+    </div>
+  );
 };
 
 export default Home;
