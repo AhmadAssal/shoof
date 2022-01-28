737058a56bb67a56f38bfb07ad57a018e4632c92
AhmadAssal <ahmadnaderassal@gmail.com>
2022-01-28 20:31:02 +0200

    feat: make genre button rounded

--- stories/GenreButton.tsx
+++ stories/GenreButton.tsx
@@ -7,7 +7,7 @@ interface GenreProps {
 
 export const GenreButton = ({ title = "The Dark Knight" }: GenreProps) => {
   return (
-    <p className="bg-gray-400 p-2 m-1 text-white w-max font-bold inline-block text-xs">
+    <p className="bg-gray-400 p-2 m-1 rounded-lg text-white w-max font-bold inline-block text-xs">
       {title}
     </p>
   );
