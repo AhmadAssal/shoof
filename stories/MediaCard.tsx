be9d3021def7ca3b99db3a8c2f23916a411366b0
AhmadAssal <ahmadnaderassal@gmail.com>
2022-01-28 18:59:20 +0200

    feat: add stars rating in MediaCard

--- stories/MediaCard.tsx
+++ stories/MediaCard.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import "tailwindcss/tailwind.css";
+import ReactStars from "react-stars";
 
 interface MediaCardProps {
-  primary?: boolean;
-
   imageUrl: string;
 
   title: string;
@@ -14,16 +13,16 @@ interface MediaCardProps {
 
   trailerUrl: string;
 
-  //availableOn
+  genres: string[];
 }
 
 export const MediaCard = ({
-  primary = true,
   title = "The Dark Knight",
   imdbRating = 9.0,
   userRating = 9.2,
   imageUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
   trailerUrl = "https://www.youtube.com/watch?v=EXeTwQWrcwY",
+  genres = ["Action", "Superhero", "Drama"],
 }: MediaCardProps) => {
   return (
     <div className="group bg-blue-600 w-max">
@@ -32,9 +31,17 @@ export const MediaCard = ({
         src={imageUrl}
         alt="The Dark Knight Poster"
       ></img>
-      <h1 className="text-white mx-auto text-center justify-center w-full my-2">
-        {title}
-      </h1>
+      <ReactStars
+        count={5}
+        value={5}
+        size={30}
+        edit={false}
+        className="justify-items-center  mx-auto w-full -my-6 flex justify-center "
+      ></ReactStars>
+      <h1 className="text-white mx-auto text-center w-full my-4">{title}</h1>
+      {/* {
+        genres.map(genre => <></>)
+      } */}
     </div>
   );
 };
