b38d9bb44b0609c55fcbcf9377ed3289a2adbf2a
AhmadAssal <ahmadnaderassal@gmail.com>
2022-01-28 19:56:02 +0200

    feat: create genre buttons

---
+++ stories/GenreButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "tailwindcss/tailwind.css";
+
+interface GenreProps {
+  title: string;
+}
+
+export const GenreButton = ({ title = "The Dark Knight" }: GenreProps) => {
+  return (
+    <p className="bg-gray-400 p-2 m-1 text-white w-max font-bold inline-block text-xs">
+      {title}
+    </p>
+  );
+};
