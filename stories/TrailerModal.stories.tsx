509db120fe5c0412daada451051e7f2485fb2e94
AhmadAssal <ahmadnaderassal@gmail.com>
2022-01-31 20:11:05 +0200

    refactor: change text to title in modal

--- stories/TrailerModal.stories.tsx
+++ stories/TrailerModal.stories.tsx
@@ -15,7 +15,7 @@ const Template: ComponentStory<typeof TrailerModal> = (args) => (
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  text: "why u do dis",
+  title: "why u do dis",
   open: true,
   onClick: () => {},
   onCloseModal: () => {},
