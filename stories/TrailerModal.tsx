1daab2aebdebb89d88da2cc6d0c4a0274fba4a6c
AhmadAssal <ahmadnaderassal@gmail.com>
2022-01-31 20:10:06 +0200

    refactor: removed default arguments

--- stories/TrailerModal.tsx
+++ stories/TrailerModal.tsx
@@ -2,28 +2,35 @@ import React from "react";
 import "tailwindcss/tailwind.css";
 import "react-responsive-modal/styles.css";
 import { Modal } from "react-responsive-modal";
-
+import { Button } from "./Button";
 interface TrailerModalProps {
-  text: string;
+  title: string;
+  url: string;
   onClick: () => void;
   open: boolean;
   onCloseModal: () => void;
 }
 
 export const TrailerModal = ({
-  text = "Add",
-  onClick = () => {},
-  open = false,
-  onCloseModal = () => {},
+  title,
+  url,
+  onClick,
+  open,
+  onCloseModal,
 }: TrailerModalProps) => {
   return (
-    <Modal open={open} onClose={onCloseModal} center>
-      <h2>Simple centered modal</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar
-        risus non risus hendrerit venenatis. Pellentesque sit amet hendrerit
-        risus, sed porttitor quam.
-      </p>
+    <Modal open={open} onClose={onCloseModal} center closeIcon>
+      <h2 className="text-2xl m-2">{title}</h2>
+      <iframe
+        width="560"
+        height="315"
+        src={url}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      <Button onClick={onClick} text="Close"></Button>
     </Modal>
   );
 };
