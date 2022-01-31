116addb5732e2acbd3aaf33895f23c6646481df6
AhmadAssal <ahmadnaderassal@gmail.com>
2022-01-31 19:43:33 +0200

    feat: add tailwind styled Modal

--- stories/TrailerModal.tsx
+++ stories/TrailerModal.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 import "tailwindcss/tailwind.css";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
 
 interface TrailerModalProps {
   text: string;
   onClick: () => void;
+  open: boolean;
+  onCloseModal: () => void;
 }
 
 export const TrailerModal = ({
   text = "Add",
   onClick = () => {},
+  open = false,
+  onCloseModal = () => {},
 }: TrailerModalProps) => {
   return (
-    <button
-      className="bg-gray-700 rounded-lg p-2 text-white m-2"
-      onClick={onClick}
-    >
-      {text}
-    </button>
+    <Modal open={open} onClose={onCloseModal} center>
+      <h2>Simple centered modal</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar
+        risus non risus hendrerit venenatis. Pellentesque sit amet hendrerit
+        risus, sed porttitor quam.
+      </p>
+    </Modal>
   );
 };
