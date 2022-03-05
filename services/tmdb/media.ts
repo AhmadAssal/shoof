fe9f7d9a5bcba7da87f1251be0bd26bbd69461f1
AhmadAssal <ahmadnaderassal@gmail.com>
2022-03-05 16:43:09 +0200

    feat: add getShow and search

--- services/tmdb/media.ts
+++ services/tmdb/media.ts
@@ -22,3 +22,49 @@ export const getMovie = async (movieId: number) => {
     };
   }
 };
+
+export const getShow = async (showId: number) => {
+  try {
+    const response = await axios.get(
+      process.env.TMDB_SERIES_URL + "/" + showId,
+      {
+        params: {
+          api_key: process.env.TMDB_API_KEY,
+          language: "en-US",
+          append_to_response: "videos",
+        },
+      }
+    );
+    return { status: "Show data retrieved", data: response.data };
+  } catch (error: any) {
+    return {
+      status: "failed to get show",
+      message: error,
+    };
+  }
+};
+
+export const search = async (query: string) => {
+  try {
+    const response = await axios.get(
+      process.env.TMDB_SEARCH_URL + "?query=" + query,
+      {
+        params: {
+          api_key: process.env.TMDB_API_KEY,
+          language: "en-US",
+        },
+      }
+    );
+    return { status: "Data retrieved", data: response.data };
+  } catch (error: any) {
+    return {
+      status: "failed to get data",
+      message: error,
+    };
+  }
+};
+
+// const call_search = async () => {
+//   console.log((await search("Spider-Man")).data.results);
+// };
+// call_search();
