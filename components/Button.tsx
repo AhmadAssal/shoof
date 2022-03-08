c7e450357b36cb7f71589df6659491a99988d427
AhmadAssal <ahmadnaderassal@gmail.com>
2022-03-09 00:26:05 +0200

    feat: add disabled attribute to button

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,13 +4,15 @@ import "tailwindcss/tailwind.css";
 interface ButtonProps {
   text: string;
   onClick: () => void;
+  disabled: boolean;
 }
 
-export const Button = ({ text = "Add", onClick = () => {} }: ButtonProps) => {
+export const Button = ({ text, onClick, disabled }: ButtonProps) => {
   return (
     <button
       className="bg-gray-700 rounded-lg p-2 text-white m-2"
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
