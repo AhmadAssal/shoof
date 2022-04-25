781e5b6dff37bc7c53041e62efcf387b3ac3a6de
AhmadAssal <ahmadnaderassal@gmail.com>
2022-04-25 23:19:19 +0200

    feat: add navbars to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-
+import { HorizontalNavbar } from "../stories/HorizontalNavbar";
+import { VerticalNavbar } from "../stories/VerticalNavbar";
 const Home: NextPage = () => {
   return (
-    <div className="container">
-      <p>We play what we are dealt</p>
+    <div className="flex flex-row grow  text-white w-screen">
+      <VerticalNavbar className="grow-0 shrink-0 basis-36 md:flex md:flex-col hidden"></VerticalNavbar>
+      <div className="flex flex-col">
+        <HorizontalNavbar></HorizontalNavbar>
+      </div>
     </div>
   );
 };
