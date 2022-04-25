05693ad0602e3bcef3d186ad71507744bb4d0d06
AhmadAssal <ahmadnaderassal@gmail.com>
2022-04-25 15:05:50 +0200

    feat: create vertical navbar storybook component

---
+++ stories/VerticalNavbar.stories copy.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { VerticalNavbar } from "./VerticalNavbar";
+export default {
+  title: "Example/VerticalNavbar",
+  component: VerticalNavbar,
+} as ComponentMeta<typeof VerticalNavbar>;
+
+const Template: ComponentStory<typeof VerticalNavbar> = (args) => (
+  <VerticalNavbar />
+);
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  text: "Action",
+};
--- stories/VerticalNavbar.tsx
+++ stories/VerticalNavbar.tsx
@@ -11,17 +11,14 @@ export const VerticalNavbar = () => {
         alt="Picture of the logo. It consists of a TV (representing series), a clapperboard (representing movies) and the word anime in Japanese"
         className=" w-fit h-full"
       ></img>
-      <a href="/html/" className="mx-4">
-        HTML
+      <a href="/cinema/" className="mx-4">
+        Cinema
       </a>
-      <a href="/css/" className="mx-4">
-        CSS
+      <a href="/tv/" className="mx-4">
+        TV
       </a>
-      <a href="/js/" className="mx-4">
-        JavaScript
-      </a>
-      <a href="/python/" className="mx-4">
-        Python
+      <a href="/anime/" className="mx-4">
+        Anime
       </a>
     </nav>
   );
