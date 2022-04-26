f84bf3b4a5c785fd7e3bc33a11aa90a7205ce34b
AhmadAssal <ahmadnaderassal@gmail.com>
2022-04-26 23:11:05 +0200

    feat: adjust centering items in main page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen w-screen">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
