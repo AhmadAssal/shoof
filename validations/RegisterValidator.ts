30ce65642b4a21db2a22d496641901fb3d51e655
AhmadAssal <ahmadnaderassal@gmail.com>
2022-04-28 00:28:29 +0200

    feat: validate email has no whitespace

--- validations/RegisterValidator.ts
+++ validations/RegisterValidator.ts
@@ -3,4 +3,11 @@ export const validator = (
   username: string,
   password: string,
   passwordConfirmation: string
-) => {};
+) => {
+  if (email.includes(" ")) {
+    if (typeof window !== "undefined") {
+      window.alert("Your email can not contain whitespace");
+    }
+    return;
+  }
+};
