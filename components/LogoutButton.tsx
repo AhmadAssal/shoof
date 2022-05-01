9124ce64320acea7815099f65586a341a7c6c973
AhmadAssal <ahmadnaderassal@gmail.com>
2022-05-01 13:49:02 +0200

    feat: implement logout button

---
+++ components/LogoutButton.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "tailwindcss/tailwind.css";
+import axios from "axios";
+import Router from "next/router";
+import { useLocalStorage } from "usehooks-ts";
+
+interface LogoutProps {
+  className: string;
+}
+export const LogoutButton = ({ className }: LogoutProps) => {
+  const onClick = async () => {
+    const shoofToken = localStorage.getItem("shoof-token")?.replaceAll('"', "");
+    if (!shoofToken) {
+      alert("You are not signed in");
+      Router.push("/login");
+    }
+    try {
+      const logoutResponse = await axios.post(
+        process.env.NEXT_PUBLIC_BACKEND! + "/api/logout",
+        {},
+        {
+          withCredentials: true,
+          headers: { Authorization: `Bearer ${shoofToken}` },
+        }
+      );
+      localStorage.removeItem("shoof-token");
+      Router.push("/login");
+    } catch (error: any) {
+      if (error.response.status === 401) {
+        alert("You are already logged out.");
+      }
+    }
+  };
+  return (
+    <button
+      className={"bg-gray-700 rounded-lg p-2 text-white m-2 "}
+      onClick={onClick}
+    >
+      Log Out
+    </button>
+  );
+};
