a9b04c5483e5aab067adce2f5c5aea9a03f42469
AhmadAssal <ahmadnaderassal@gmail.com>
2022-05-04 21:09:32 +0200

    feat: implement reset password service

---
+++ services/authentication/resetpassword.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const resetPassword = async (
+  email: string,
+  token: string,
+  password: string
+) => {
+  const userData = {
+    email,
+    newPassword: password,
+  };
+  const url = process.env.NEXT_PUBLIC_RESET_PASSWORD! + token;
+  console.log();
+  const resetPasswordResponse = await axios.post(url, userData);
+  return resetPasswordResponse;
+};
