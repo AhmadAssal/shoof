5756a464057d33b38de0d8dc1dc4790b2aaa5ee1
AhmadAssal <ahmadnaderassal@gmail.com>
2022-05-16 16:41:23 +0200

    feat: redirect to login page if not signed in

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,18 @@ import Head from "next/head";
 import Image from "next/image";
 import { HorizontalNavbar } from "../components/HorizontalNavbar";
 import { VerticalNavbar } from "../components/VerticalNavbar";
+import { hasToken } from "../utils/HasToken";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 const Home: NextPage = () => {
+  useEffect(() => {
+    if (!hasToken()) {
+      router.push("/login");
+    }
+  }, []);
+  const router = useRouter();
+
   return (
     <div className="flex flex-row grow  text-white w-screen">
       <VerticalNavbar className="grow-0 shrink-0 basis-36 md:flex md:flex-col hidden"></VerticalNavbar>
