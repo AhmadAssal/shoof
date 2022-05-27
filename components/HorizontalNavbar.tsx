5056c088eced6e5f96d3859129788b1495d0c957
AhmadAssal <ahmadnaderassal@gmail.com>
2022-05-27 21:20:41 +0200

    refactor: move token checking code to CSR

--- components/HorizontalNavbar.tsx
+++ components/HorizontalNavbar.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "tailwindcss/tailwind.css";
 import { LogoutButton } from "./LogoutButton";
 import { hasToken } from "../utils/HasToken";
+import { useState } from "react";
 export const HorizontalNavbar = () => {
+  const [token, setToken] = useState<boolean | undefined>(false);
+  useEffect(() => {
+    const tokenExists = hasToken();
+    setToken(tokenExists);
+  }, []);
   return (
     <nav className="text-white flex h-16 w-full text-center items-center sticky top-0 bg-black z-10">
       <a href="/cinema/" className="mx-4">
@@ -14,11 +20,7 @@ export const HorizontalNavbar = () => {
       <a href="/anime/" className="mx-4">
         Anime
       </a>
-      {hasToken() ? (
-        <LogoutButton className="ml-auto mr-8"></LogoutButton>
-      ) : (
-        <></>
-      )}
+      {token ? <LogoutButton className="ml-auto mr-8"></LogoutButton> : <></>}
     </nav>
   );
 };
